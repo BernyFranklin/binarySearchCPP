7235cf02fd0b7b93414a93f8f08c02527bab52c5
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-03-31 08:35:25 -0700

    Initial Commit
    Added title header

---
+++ src/binarySearchCPP/binarySearchCPP/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * binarySearchCPP Version#1.0
+ * Frank Bernal
+ * CIS 054 C/C++ Programming
+ * Input: Name or Phone number
+ * Output: Name or Phone number
+ * 31 Mar 2022
+ */
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
